f9fb8f54c6fd4ce7aaa418079fdd1d82f9be5d09
AdamMc331 <amcneilly331@gmail.com>
2018-06-17 00:19:03 -0400

    Added endless scrolling listview of random words.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'random_words.dart';
 
 void main() => runApp(new MyApp());
@@ -7,18 +6,9 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-
     return new MaterialApp(
-      title: "Welcome to Flutter",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Welcome to Flutter"),
-        ),
-        body: new Center(
-          child: new RandomWords(),
-        ),
-      ),
+      title: "Startup Name Generator",
+      home: new RandomWords(),
     );
   }
 }
--- lib/random_words.dart
+++ lib/random_words.dart
@@ -7,11 +7,53 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  /// Builds a list of suggested word pairings in a ListView.
+  ///
+  /// The itemBuilder callback is called once per suggested
+  /// word pairing, and places each suggestion into a ListTile row.
+  /// For even rows, the function adds a ListTile row for the word pairing.
+  /// For odd rows, the function adds a Divider widget to visually separate the entries.
+  /// Note that the divider may be difficult to see on smaller defices.
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+        // The syntax "index ~/ 2" divides i by 2 and returns an integer result.
+        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+        // This calculates the actual number of word pairings in the ListView, minus divider widgets.
+        final index = i ~/ 2;
+
+        // If we've reached the end of word pairings, generate 10 more
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  /// Builds a ListTile for an individual WordPair row.
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    final textWidget = Text(wordPair.asPascalCase);
-
-    return textWidget;
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Startup Name Generator"),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
